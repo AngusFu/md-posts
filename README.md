1c565d64042fe1237c114101072a1211f0c189e5
fujun-iri <fujun-iri@360.cn>
2016-07-15 13:46:45 +0800

    'commit'

--- README.md
+++ README.md
@@ -8,43 +8,42 @@ Your [contact](mailto:angusfu1126@qq.com) is appreciated if any question/problem
 
 You can also see [my blog online](https://segmentfault.com/blog/wemlin).
 
+## translation
 
-## translation 
+- [盘点即将影响世界的九大创新](translation/9-innovations-that-could-become-the-next-big-thing.md)
 
-- [NO WEBSOCKETS OVER HTTP/2](./translation/no-websockets-over-http2.md)
+- [给 Service Worker 开后门](translation/backdoor-service-worker.md)
 
-- [客户端存储](./translation/client-side-storage.md)
+- [客户端存储](translation/client-side-storage.md)
 
-- [Node.js 中的队列](./translation/queues-in-node.js.md)
+- [无限滚动加载最佳实践](translation/infinite-scrolling-best-practices.md)
 
-- [两百行内 JavaScript 打造lambda 演算解释器](./translation/writing-a-lambda-calculus-interpreter-in-javascript.md)
+- [No WebSockets Over HTTP/2](translation/no-websockets-over-http2.md)
 
-- [无限滚动加载最佳实践](./translation/infinite-scrolling-best-practices.md)
+- [防火墙背后的笔记：Web 设计在中国](translation/notes-from-behind-the-firewall-the-state-of-web-design-in-china.md)
 
-- [防火墙背后的笔记：Web 设计在中国](./translation/notes-from-behind-the-firewall-the-state-of-web-design-in-china.md)
+- [Node.js 中的队列](translation/queues-in-node.js.md)
 
-- [测试驱动开发：使用 Node.js 和 MongoDB 构建 Todo API ](./translation/todo-api-with-unit-test.md)
+- [测试驱动开发：使用 Node.js 和 MongoDB 构建 Todo API](translation/todo-api-with-unit-test.md)
 
-- [给 Service Worker 开后门](./translation/backdoor-service-worker.md)
+- [WebP 支持：超出你想象](translation/webp-support.md)
 
-- [盘点即将影响世界的九大创新](./translation/9-innovations-that-could-become-the-next-big-thing.md)
-
-- [WebP 支持：超出你想象](./translation/webp-support.md)
+- [两百行内 JavaScript 打造lambda 演算解释器](translation/writing-a-lambda-calculus-interpreter-in-javascript.md)
 
 ## notes & articles
 
-- [关于移动端 rem 布局的一些总结](./blog/about-rem-layout.md)
+- [关于移动端 rem 布局的一些总结](blog/about-rem-layout.md)
 
-- [Chrome和Firefox下的网页截图](./blog/screen-shot-upload.md)
+- [react-native 开发 App 手记](blog/app-with-react-native.md)
 
-- [近来在Firefox上遇到的一些坑](./blog/problems-with-firefox.md)
+- [关于JavaScript 函数传参](blog/javascript-argument.md)
 
-- [react-native 开发 App 手记](./blog/app-with-react-native.md)
+- [【nshm】 Node.js .sh scripts Manager](blog/Node.js-sh-scripts-Manager.md)
 
-- [关于JavaScript 函数传参](./blog/javascript-argument.md)
+- [近来在Firefox上遇到的一些坑](blog/problems-with-firefox.md)
 
-- [使用 ava 和 jsdom 测试前端界面](./blog/test-with-ava-jsdom.md)
+- [Chrome和Firefox下的网页截图](blog/screen-shot-upload.md)
 
-- [判断资源并行加载完成的三种办法：计数、Promise及 $.Deferred](./blog/when-are-all-resources-all-loaded.md)
+- [使用 ava 和 jsdom 测试前端界面](blog/test-with-ava-jsdom.md)
 
-- [Node.js .sh scripts Manager](./blog/Node.js-sh-scripts-Manager.md)
+- [判断资源并行加载完成的三种办法：计数、Promise及 $.Deferred](blog/when-are-all-resources-all-loaded.md)
---
+++ index.js
@@ -0,0 +1,223 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var path = require('path');
+var exec = require('child_process').exec;
+
+var argvs = Array.from(process.argv).slice(2);
+var category = argvs[0];
+var filename = argvs[1];
+
+var dbjsonPath = './db.json';
+checkDB(dbjsonPath);
+var DBData = require(dbjsonPath),
+    translations = DBData['translation'],
+    articles = DBData['article'];
+
+if (!translations) {
+    translations = DBData['translation'] = [];
+}
+
+if (!articles) {
+    articles = DBData['article'] = [];
+}
+
+// post -t [name]
+if (category === '-t' && filename) {
+
+    initDirectory({
+        dirname: 'translation',
+        filename: filename
+    });
+
+    translations.push({
+        name: filename,
+        createAt: +new Date
+    });
+
+    return saveDB(dbjsonPath, DBData);
+}
+
+// post -a [name]
+if (category === '-a' && filename) {
+
+    initDirectory({
+        dirname: 'blog',
+        filename: filename
+    });
+
+    articles.push({
+        name: filename,
+        createAt: +new Date
+    });
+
+    return saveDB(dbjsonPath, DBData);
+}
+
+
+// deploy to github
+// post -d
+if (category === '-d') {
+    collectMeta(translations, './translation');
+    collectMeta(articles, './blog');
+    rewriteReadMe({
+        translation: translations,
+        'notes & articles': articles
+    });
+
+    saveDB(dbjsonPath, DBData);
+    exec(`npm run deploy`);
+    return;
+}
+
+// init db.json with current files
+// post -i
+if (category === '-i') {
+    updateCurrentDirectory({
+        './translation': translations,
+        './blog': articles
+    });
+    rewriteReadMe({
+        translation: translations,
+        'notes & articles': articles
+    });
+    return saveDB(dbjsonPath, DBData);
+}
+
+
+// else
+console.log(
+`Usage: 
+  > post -t [name]    --- add translations
+  > post -a [name]    --- add articles
+  > post -d           --- deploy
+  > post -i           --- init db.json with current files
+`);
+
+
+
+// ========================================================
+
+function joinp() {
+    [].unshift.call(arguments, __dirname);
+    return path.join.apply(path, arguments);
+}
+
+
+// ========================================================
+
+function checkDB(dbPath) {
+    var stat = null,
+        dbObj = {};
+    
+    dbPath = joinp(dbPath);
+
+    try {
+        stat = fs.statSync(dbPath);
+    } catch (e) {}
+
+    if (!stat) {
+        fs.writeFileSync(dbPath, '{}', 'utf-8');
+    }
+}
+
+function saveDB(dbPath, DBData) {
+    fs.writeFileSync(dbPath, JSON.stringify(DBData, null, 4), 'utf-8');
+}
+
+// ===========================================================
+
+function makeDirIfNotExists(path) {
+    var stat = null;
+    try {
+        stat = fs.statSync(path);
+    } catch (e) {}
+
+    if (!stat || !stat.isDirectory()) {
+        fs.mkdirSync(path);
+    }
+}
+
+function writeBlankFile(filename) {
+    fs.writeFileSync(filename, '', 'utf-8');
+}
+
+function initDirectory(config) {
+    var dirname = config.dirname,
+        filename = config.filename,
+        type = config.type || 'md';
+    makeDirIfNotExists(joinp(dirname));
+    writeBlankFile(joinp(dirname, filename + '.' + type));
+}
+
+
+// ===========================================================
+
+
+function collectMeta(collection, dir) {
+    var H1_RE = /(?:\n|^)#\s*([^\n#]+)\s*\n/;
+    collection.forEach(function(item) {
+        try {
+            // TODO
+            // no more repeat
+            var content = fs.readFileSync(joinp(dir, item.name + '.md')).toString(),
+                macth = content.match(H1_RE),
+                title = macth && macth[1];
+            
+            if (!title) return;
+            
+            item.title = title.trim();
+            item.url = path.join(dir, item.name + '.md').replace(/\\/g, '/');
+        } catch (e) {}
+    });
+}
+
+
+function rewriteReadMe(obj) {
+    var content = `# blog
+
+I am Wemlin(文蔺) from China, now working as a web developer. 
+
+This repository will witness my progress & growth in this industry.
+
+Your [contact](mailto:angusfu1126@qq.com) is appreciated if any question/problem.
+
+You can also see [my blog online](https://segmentfault.com/blog/wemlin).`;
+
+    Object.keys(obj).forEach(function(key) {
+        var arr = obj[key];
+        content += `\n\n## ${key}`
+        arr.forEach(function(item) {
+            content += `\n\n- [${item.title}](${item.url})`;
+        });
+    });
+
+    fs.writeFileSync('./README.md', content, 'utf-8');
+}
+
+function updateCurrentDirectory(obj) {
+    var H1_RE = /(?:\n|^)#\s*([^\n#]+)\s*\n/;
+
+    Object.keys(obj).forEach(function(dirname) {
+        makeDirIfNotExists(joinp(dirname));
+
+        var data = obj[dirname],
+            files= fs.readdirSync(joinp(dirname));
+        
+        data.length = 0;
+
+        files.forEach(function(filename) {
+            var content = fs.readFileSync(joinp(dirname, filename)).toString(),
+                macth = content.match(H1_RE),
+                title = macth && macth[1];
+
+            if (!title) return;
+
+            data.push({
+                name: filename.slice(0, -3),
+                url: dirname + '/' + filename + '.md',
+                title: title.trim()
+            });
+        });
+    });
+}
